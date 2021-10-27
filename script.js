c64e6d0bb6f9508285197383cfb23d2ab9f96c24
IamMM <m.maske@posteo.de>
2021-10-27 11:04:04 +0200

    add animated top button
    add animated top button

---
+++ script.js
@@ -0,0 +1,7 @@
+//Get the top button:
+mybutton = document.getElementById("topBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
