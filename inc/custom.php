189fc0f65fd8a4ae99b3d9f44805e407142ea852
IamMM <m.maske@posteo.de>
2022-02-01 19:02:42 +0100

    dynamic content partner:innen

--- inc/custom.php
+++ inc/custom.php
@@ -1,7 +1,20 @@
 <?php
-/* 
+/*
 This file is included into functions.php
-Use this file to add custom PHP code to functions.php 
+Use this file to add custom PHP code to functions.php
 */
 
 ?>
+
+<?php function logo_loop() {
+  $images = get_field('logo_gallery');
+  console.log($images);
+  $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
+  if( $images ): ?>
+      <?php foreach( $images as $image ): ?>
+        <div class="swiper-slide">
+          <img src=" <?php echo esc_url($image['url']); ?>" class="h-16 mx-auto"/>
+        </div>
+      <?php endforeach; ?>
+  <?php endif;
+}; ?>
