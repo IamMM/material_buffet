cd7962527df96557bcc95271becea0423564fd82
IamMM <m.maske@posteo.de>
2022-02-14 14:52:18 +0100

    deprecated

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Get the top button:
-mybutton = document.getElementById("topBtn");
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   window.scrollTo({top: 0, behavior: 'smooth'});
