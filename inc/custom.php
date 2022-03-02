8a423ad003164c0574014fbbf473286423271523
IamMM <m.maske@posteo.de>
2022-03-02 11:43:29 +0100

    BUG: deactivate acf gallery loop

--- inc/custom.php
+++ inc/custom.php
@@ -4,15 +4,4 @@ This file is included into functions.php
 Use this file to add custom PHP code to functions.php
 */
 
-?>
 
-<?php function gallery_loop($field_name) {
-  $images = get_field($field_name);
-  if( $images ): ?>
-      <?php foreach( $images as $image ): ?>
-        <div class="swiper-slide">
-          <img src=" <?php echo esc_url($image['url']); ?>" class="h-full mx-auto"/>
-        </div>
-      <?php endforeach; ?>
-  <?php endif;
-}; ?>
